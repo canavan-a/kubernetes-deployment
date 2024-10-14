ce541b2cdefa29bf1fb5d380e828ad62cfcc7e98
canavan-a <aidan.canavan3@gmail.com>
2024-10-14 13:11:45 -0400

    added runtime install, no longer need kubectl install because of it

--- init.sh
+++ init.sh
@@ -1,15 +1,11 @@
+# get kubernetes runtime: (k3s)
+# k3s comes with kubectl baked in
+curl -sfL https://get.k3s.io | sh -
+sudo systemctl status k3s
+sudo k3s kubectl get nodes
 
-# get supplies needed to use kubectl (x86 architecture)
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
-# get the checksum
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-
-# validate the sha
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-
-# install kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# add permission for kubectl 
+sudo ln -s /usr/local/bin/k3s /usr/local/bin/kubectl
 
 kubectl version --client
 
