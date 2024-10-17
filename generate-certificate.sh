2559eadb56b1cdd6b6784dcc8b3b4fa9737f6713
canavan-a <aidan.canavan3@gmail.com>
2024-10-16 23:05:38 -0400

    manual certificate

---
+++ generate-certificate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Step 1: Generate a self-signed TLS certificate
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout my-cert.key \
+    -out my-cert.crt \
+    -subj "/CN=*.acanavan.com/O=My Company/C=US"
+
+# Step 2: Create a Kubernetes secret with the certificate and key
+kubectl create secret tls acanavan-manual-tls \
+    --cert=my-cert.crt \
+    --key=my-cert.key \
+    --namespace acanavan-namespace
+
+# Optional: Clean up the generated files if you don't need them locally
+rm my-cert.key my-cert.crt
